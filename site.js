626ef80d9617786708af7e2b3883da9e63a9e490
Jeff Fitch <jfitch360@gmail.com>
2026-01-29 13:26:25 -0600

    added welcome message via javascript

--- site.js
+++ site.js
@@ -1,1 +1,29 @@
-console.log('Hi')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcomeMessages=[
+    'Guten Morgen! (Good Morning)',
+    'Bon Après-midi! (Good Afternoon)',
+    'Buenas Noches! (Good Evening)'
+]
+const welcome=document.querySelector('#welcome')
+const marquee=document.createElement('marquee')
+
+welcome.append(marquee)
+marquee.style.fontSize="40px"
+if(isMorning)
+{
+    marquee.innerHTML=welcomeMessages[0]
+}
+if(isAfternoon)
+{
+    marquee.innerHTML=welcomeMessages[1]
+}
+if(isMorning)
+{
+    marquee.innerHTML=welcomeMessages[2]
+}
+
